f23a9470f180e5eea0109587ffb61ab8b9a346e9
aaronwb <zmtzwb@gmail.com>
2020-12-01 13:51:22 +0800

    add ts support

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,15 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
   },
   devtool: 'inline-source-map',
   plugins: [
